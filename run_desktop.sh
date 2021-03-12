62d5d70501655f2fb5779424486e4ccfd88703e1
Alou <ablo.kces@gmail.com>
2021-03-12 17:20:04 +0000

    Desktop release and debug script lunchers (run_desktop.sh and run_desktop_debug.sh)

---
+++ run_desktop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export MWM_RESOURCES_DIR=./data/
+export MWM_WRITABLE_DIR=./data/
+
+ app=MAPS.ME
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	app=MAPS.ME.app/Contents/MacOS/MAPS.ME
+fi
+../omim-build-release/$app
